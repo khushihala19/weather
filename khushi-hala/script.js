012ee6b7b60771483df7a79e73b043fda6703f6d
khushihala19 <148927507+khushihala19@users.noreply.github.com>
2024-07-16 09:45:05 +0530

    Add files via upload

---
+++ khushi-hala/script.js
@@ -0,0 +1,52 @@
+const weatherForm = document.getElementById('weatherForm');
+const cityInput = document.getElementById('cityInput');
+const weatherResults = document.getElementById('weatherResults');
+
+const API_KEY = '217d9466fcece181f108ef7574f237b4'; // Replace with your OpenWeatherMap API key
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+
+
+weatherForm.addEventListener('submit', async function(event) {
+    event.preventDefault();
+    
+    const city = cityInput.value.trim();
+
+  
+    
+    if (city) {
+
+        try {
+            const response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+            const data = await response.json();
+
+            if (data.cod === 200) {
+                const weather = data.weather[0].main;
+                const temp = data.main.temp;
+                const humidity = data.main.humidity;
+                
+                weatherResults.innerHTML = `
+                    <h3>Weather in ${city}</h3>
+                    <p><strong>Weather:</strong> ${weather}</p>
+                    <p><strong>Temperature:</strong> ${temp} &deg;C</p>
+                    <p><strong>Humidity:</strong> ${humidity}%</p>
+                `;
+                let icon = document.getElementById("icon");
+
+                let iconurl =
+                  "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+            
+                icon.src = iconurl;
+            } 
+            
+            else {
+                weatherResults.innerHTML = `<p>${data.message}</p>`;
+            }
+        } catch (error) {
+            console.error('Error fetching weather data:', error);
+            weatherResults.innerHTML = '<p>Failed to fetch weather data. Please try again later.</p>';
+        }
+    } else {
+        weatherResults.innerHTML = '<p>Please enter a city name.</p>';
+    }
+});
